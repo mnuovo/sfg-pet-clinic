654c8489e41ee7e3db73782c2f31f441a0e2390e
Michele.Nuovo <michele.nuovo@embedit.cz>
2019-07-29 16:27:09 +0200

    Adding base entity. closes #10

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,4 +1,8 @@
 package guru.springframework.sfgpetclinic.model;
 
 public class Owner extends Person {
+
+    public Owner(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
 }
